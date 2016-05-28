3db6f1bbdadf69cd3eeffec048f37b13d56ac8a1
nichoth <nichoth@gmail.com>
2016-05-28 15:26:43 -0700

    comments

--- example/index.js
+++ example/index.js
@@ -11,9 +11,11 @@ store(function onChange(state) {
 // get current state
 console.log(store())
 
+// return an object that will xtend `oldState`
 store.on('data', function(action, oldState) {
   return action
 })
 
+// pass something to the 'data' listener
 store.emit('data', { test: 'party' })
 
